b4a00df13404c5d8c7a5d9ba1fa724923dc310d2
Rennosmx <rennomx@hotmail.com>
2020-06-22 23:00:40 -0300

    Adicionando projeto de app JoKenPo

---
+++ android/app/src/main/java/rennomelo/cursoflutter/jokenpo/MainActivity.java
@@ -0,0 +1,6 @@
+package rennomelo.cursoflutter.jokenpo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Jogo.dart
@@ -0,0 +1,116 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+
+  var _imagemApp = AssetImage("images/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo:";
+
+  void _opcaoSelecionada(String escolhaUsuario){
+    
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numeroAleatorio = Random().nextInt(3);
+    var escolhaApp = opcoes[numeroAleatorio];
+
+    switch( escolhaApp ){
+      case "pedra":
+        setState(() {
+          this._imagemApp = AssetImage("images/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          this._imagemApp = AssetImage("images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this._imagemApp = AssetImage("images/tesoura.png");
+        });
+        break;
+    }
+
+    if( (escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra") ){
+      setState(() {
+        this._mensagem = "Parabéns! Você ganhou :)";
+      });
+    }
+    else if( (escolhaUsuario == "tesoura" && escolhaApp == "pedra") ||
+             (escolhaUsuario == "pedra" && escolhaApp == "papel") ||
+             (escolhaUsuario == "papel" && escolhaApp == "tesoura")  ){
+      setState(() {
+        this._mensagem = "Você perdeu :(";
+      });
+    }
+    else{
+      setState(() {
+        this._mensagem = "Empate!";
+      });
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JoKenPo"),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha do App:",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+          ),
+          Image(image: this._imagemApp),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              this._mensagem,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra"),
+                child: Image.asset("images/pedra.png", height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("papel"),
+                child: Image.asset("images/papel.png", height: 100,),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura"),
+                child: Image.asset("images/tesoura.png", height: 100,),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
